0ae65551e4b7d1d06ccf8c60a7212ae4b14ff7c9
robi00 <bonaccorsoroby@gmail.com>
2021-09-06 12:25:05 +0200

    Activity main (classe e layout)

--- app/src/main/java/com/example/apphelp/MainActivity.kt
+++ app/src/main/java/com/example/apphelp/MainActivity.kt
@@ -4,12 +4,19 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.database.Cursor
 import android.location.Location
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.telephony.SmsManager
 import android.util.Log
+import android.widget.CompoundButton
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.github.florent37.runtimepermission.kotlin.askPermission
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -21,23 +28,36 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_main.*
+import java.math.RoundingMode.valueOf
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.concurrent.thread
+import kotlin.concurrent.timer
+
+
+var myLongitude: Double = 0.0
+var myLatitude: Double = 0.0
+lateinit var mp: MediaPlayer
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private var mMap: GoogleMap?= null
     lateinit var  mapView: MapView
     private val MAP_VIEW_BUNDLE_KEY= "MapViewBundleKey"
-
     private val DEFAULT_ZOOM = 15f
     private var fusedLocationProviderClient: FusedLocationProviderClient? = null
 
+    lateinit var mDatabaseHelper: DatabaseHelper
+
+
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap?) {
         mapView.onResume()
         mMap = googleMap
 
         askPermissionLocation()
 
-        //si vede se si ha il permesso di localizzare telefono
+        //it checks if it has permission to locate the phone
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -55,15 +75,26 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // when the user clicks on the "where am I?" button the function getCurrentLocation is called
         buttonLoc.setOnClickListener{
             getCurrentLocation()
         }
-        buttonSOS.setOnClickListener {
+        // when the user clicks on the "settings" button the function Activity2 is called
+        buttonSett.setOnClickListener {
             val m_intent = Intent(this@MainActivity, Activity2::class.java)
             startActivity(m_intent)
         }
-
-        mapView = findViewById<MapView>(R.id.map1)
+        // when the user clicks on the "SOS" button the functions sendSms and reproduceSound are called
+        buttonSOS.setOnClickListener {
+            sendSms()
+            reproduceSound()
+        }
+        // when the user clicks on the "stop sound" the function stopSound is called
+        buttSound.setOnClickListener {
+            stopSound()
+        }
+        mapView = findViewById(R.id.map1)
 
         var mapViewBundle: Bundle? = null
         if (savedInstanceState != null) {
@@ -71,6 +102,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
         mapView.onCreate(mapViewBundle)
         mapView.getMapAsync(this)
+
+        mDatabaseHelper = DatabaseHelper(this)
 
     }
 
@@ -86,24 +119,24 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mapView.onSaveInstanceState(mapViewBundle)
     }
 
-    //se non si ha il permesso di localizzare, il telefono chiede a utente di concederglielo
+    //if the phone does not have permission to locate, it asks the user to grant it
     private fun askPermissionLocation() {
         askPermission(
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION
         ) {
+            getCurrentLocation() //if the permission is granted it calls the function getCurrentLocation()
 
-            getCurrentLocation()
-        }.onDeclined{e->
+        }.onDeclined{e-> //if the permission isn't granted
             if(e.hasDenied()) {
                 e.denied.forEach {
                 }
-                AlertDialog.Builder(this)
+                AlertDialog.Builder(this) // creates a pop up to ask the user to give the permission
                     .setMessage("Please accept permissions. Otherwise you will not be able to use some of our important features")
-                    .setPositiveButton("yes") { _, _ ->
+                    .setPositiveButton("yes") { _, _ -> //if the user click on "yes", askPermission
                         e.askAgain()
                     }
-                    .setNegativeButton("no") { dialog, _ ->
+                    .setNegativeButton("no") { dialog, _ -> //if the user click on "no", the pop up closes without performing any action
                         dialog.dismiss()
                     }
                     .show()
@@ -111,26 +144,28 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    @SuppressLint("MissingPermission")
     private fun getCurrentLocation() {
         fusedLocationProviderClient =
             LocationServices.getFusedLocationProviderClient(this@MainActivity)
 
         try {
-            @SuppressLint("MissingPermission") val location =
-                fusedLocationProviderClient!!.getLastLocation()
+            val location = fusedLocationProviderClient!!.lastLocation
 
             location.addOnCompleteListener(object : OnCompleteListener<Location> {
                 override fun onComplete(loc: Task<Location>) {
                     if (loc.isSuccessful) {
-                        val currentLocation = loc.result as Location?
+                        val currentLocation = loc.result
                         if(currentLocation != null) {
-                            moveCamera(//to move map tu current location
+                            moveCamera(//to move map to current location
                                 LatLng(currentLocation.latitude, currentLocation.longitude),
                                 DEFAULT_ZOOM
                             )
                         }
+                        myLongitude = currentLocation.longitude
+                        myLatitude = currentLocation.latitude
                     }else {
-                        askPermissionLocation()
+                        askPermissionLocation() //if the phone does not have permission to locate, it asks the user to grant it
                     }
                 }
             })
@@ -140,6 +175,76 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
     private fun moveCamera(latLng: LatLng, zoom: Float){
         mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom))
+    }
+
+    private fun sendSms() {
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) +
+                    ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS))
+            != PackageManager.PERMISSION_GRANTED) { //if the phone does not have permission to send sms, it asks the user to grant it
+            askPermissionSms()
+        } else {
+            thread { // a thread is used so you can send sms every x time with the updated position
+                var phone: Cursor = mDatabaseHelper.getData()
+                var listPhone: ArrayList<String> = ArrayList()
+                if(phone.moveToFirst()) {
+                    do{
+                    listPhone.add(phone.getString(2))
+                    }while (phone.moveToNext())
+                }
+
+                var obj = SmsManager.getDefault()
+
+                getCurrentLocation() //per prendere la posizione corrente (per sms)
+
+                for(k in 0..5) {
+                    var i = listPhone.size
+                    var j = 0
+                    while (j < i) {
+                        var number = listPhone[j]
+                        obj.sendTextMessage("$number",
+                            null,
+                            "HELP ME, my position is: \n $myLatitude, $myLongitude ",
+                            null,
+                            null)
+                        j++
+                    }
+                    Thread.sleep(60000)
+                } //5 messages are sent to each emergency contact, one message per minute
+            }
+        }
+    }
+    //if the phone does not have permission to send sms, it asks the user to grant it
+    private fun askPermissionSms() {
+        askPermission(
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.READ_SMS
+        ) {
+            sendSms() //if the permission is granted it calls the function sendSms()
+        }.onDeclined{e->
+            if(e.hasDenied()) { //if the permession isn't granted
+                e.denied.forEach {
+                }
+                AlertDialog.Builder(this) //create a pop up to ask the user to give the permission
+                    .setMessage("Please accept permissions. Otherwise you will not be able to use some of our important features")
+                    .setPositiveButton("yes") { _, _ -> //if the user click on "yes", askPermission
+                        e.askAgain()
+                    }
+                    .setNegativeButton("no") { dialog, _ -> //if the user click on "no", the pop up closes without performing any action
+                        dialog.dismiss()
+                    }
+                    .show()
+            }
+        }
+    }
+    //to play the sound imported into resources
+    private fun reproduceSound() {
+        mp = MediaPlayer.create(this, R.raw.police)
+        mp.start()
+
+    }
+    //to stop the sound
+    private fun stopSound() {
+        mp.stop()
     }
 }
 
