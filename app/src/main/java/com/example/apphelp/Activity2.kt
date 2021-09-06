459a61816c4190e5516fd077db598af8f5539a56
Lorenzo <lorenzo.meregalli.work@gmail.com>
2021-09-06 12:23:50 +0200

    Activity2 (classe e layout)

--- app/src/main/java/com/example/apphelp/Activity2.kt
+++ app/src/main/java/com/example/apphelp/Activity2.kt
@@ -3,20 +3,21 @@ package com.example.apphelp
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.database.Cursor
-import android.database.sqlite.SQLiteDatabase
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
-import android.widget.Toast
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.apphelp.databinding.Activity2Binding
+import kotlinx.android.synthetic.main.activity_2.*
 
 
 class Activity2 : AppCompatActivity() {
-
-    lateinit var databaseHelper : DataBaseHelper
 
     //view binding
     lateinit var binding: Activity2Binding
@@ -25,32 +26,36 @@ class Activity2 : AppCompatActivity() {
     //contact pick code
     private val CONTACT_PICK_CODE = 2
 
+    lateinit var mDatabaseHelper: DatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        databaseHelper = DataBaseHelper (this)
-        var sqLiteDataBase : SQLiteDatabase = databaseHelper.writableDatabase
-
-
         binding = Activity2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        mDatabaseHelper = DatabaseHelper(this)
+
+        populateListView()
+
 
         //handler click to pick contact
-        binding.addFab.setOnClickListener {
+        add_data.setOnClickListener {
             //check permission allowed or not
             if(chechConstactPermission()) {
                 //allowed
                 pickContact()
+                populateListView()
             }
             else{
                 //not allowed, request
                 requestContactPermission()
             }
         }
+
     }
 
     private fun chechConstactPermission(): Boolean{
-        //check if permission was granted/alloews or not, returns true if allowed, false if not
+        //check if permission was granted/allowes or not, returns true if allowed, false if not
         return ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED
     }
     private fun requestContactPermission(){
@@ -58,7 +63,7 @@ class Activity2 : AppCompatActivity() {
         val permission = arrayOf(android.Manifest.permission.READ_CONTACTS)
         ActivityCompat.requestPermissions(this,permission, CONTACT_PERMISSION_CODE)
     }
-    private fun pickContact(){
+    fun pickContact(){
         //intent to pick contact
         val intent = Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI)
         startActivityForResult(intent, CONTACT_PICK_CODE)
@@ -84,9 +89,7 @@ class Activity2 : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        var nameList: MutableList<String> = mutableListOf()
-        var idList: MutableList<String> = mutableListOf()
-        var numberList: MutableList<String> = mutableListOf()
+
         //handle intent result. calls when user from intent (pickContact) picks on cancels pick contact
         if(resultCode == RESULT_OK) {
             //calls when user click a contact from contacts (intent) list
@@ -103,22 +106,20 @@ class Activity2 : AppCompatActivity() {
                     //get contacts details
                     val contactId = cursor1.getString(cursor1.getColumnIndex(ContactsContract.Contacts._ID))
                     val contactName = cursor1.getString(cursor1.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
-                    val contactThumbnail = cursor1.getString(cursor1.getColumnIndex(ContactsContract.Contacts.PHOTO_THUMBNAIL_URI))
+                    val contactImm = cursor1.getString(cursor1.getColumnIndex(ContactsContract.Contacts.PHOTO_THUMBNAIL_URI))
                     val idResults = cursor1.getString(cursor1.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))
                     val idResultHold = idResults.toInt()
-
-                    idList.add(contactId)
-                    nameList.add(contactName)
 
                     //set details
                     binding.contactIv.append("ID: $contactId")
                     binding.contactIv.append("\nName: $contactName")
+
                     //set image, first check if uri/thumbnail is not null
-                    if(contactThumbnail != null){
-                        binding.thumbnailIv.setImageURI(Uri.parse(contactThumbnail))
+                    if(contactImm != null){
+                        binding.immContact.setImageURI(Uri.parse(contactImm))
                     }
                     else {
-                        binding.thumbnailIv.setImageResource(R.drawable.ic_person)
+                        binding.immContact.setImageResource(R.drawable.ic_person)
                     }
                     //check if contact has a phone number or not
                     if(idResultHold == 1) {
@@ -131,9 +132,21 @@ class Activity2 : AppCompatActivity() {
                         while(cursor2!!.moveToNext()) {
                             //get phone number
                             val contactNumber = cursor2.getString(cursor2.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-                            //set phone number
-                            binding.contactIv.append("\nPhone: $contactNumber")
-                            numberList.add(contactNumber)
+                            var check = mDatabaseHelper.checkDuplicate(contactNumber)
+                            if (check) {
+                                Log.d("ActivityResult", "twice the same number")
+                            }else{
+                                //set phone number
+                                binding.contactIv.append("\nPhone: $contactNumber")
+
+                                var newEntryName: String = contactName.toString() //contains the name of the selected contact
+                                var newEntryPhone: String = contactNumber.toString() //contains the phone number of the selected contact
+                                if(newEntryName.isNotEmpty() && newEntryPhone.isNotEmpty()) {
+                                    addData(newEntryName,newEntryPhone)
+                                } else {
+                                    toastMessage("You must select a contact!")
+                                }
+                            }
                         }
                         cursor2.close()
                     }
@@ -143,7 +156,71 @@ class Activity2 : AppCompatActivity() {
         }
         else{
             //cancelled picking contact
-            Toast.makeText(this,"Cancelled",Toast.LENGTH_SHORT).show()
+            toastMessage("Cancelled")
+        }
+    }
+    fun addData(newEntryName: String, newEntryPhone: String){
+        //check if the selected telephone number is already present in the database,
+        // so if the user had already entered this contact among the emergency ones
+        var check = mDatabaseHelper.checkDuplicate(newEntryPhone)
+        if (check) { //if checkDuplicate return true (so if that contact is already saved in the db)
+            toastMessage("This contact is already present among the emergency ones")
+        }else{
+            //passes the name and phone number of the selected contact to the database to insert them
+            var insertData: Boolean = mDatabaseHelper.addData(newEntryName, newEntryPhone)
+            if(insertData) { //if insertData return true
+                toastMessage("Data Successfully Inserted!")
+            } else { //return false
+                toastMessage("Something went wrong")
+            }
+        }
+    }
+    private fun toastMessage(message: String) {
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+    }
+
+    private fun populateListView() {
+        //get the data and append to the list
+        var data: Cursor = mDatabaseHelper.getData()
+        var listData: ArrayList<String> = ArrayList()
+        while(data.moveToNext()) {
+            //get the value from the database in column 1 (name)
+            //then add it to the ArrayList
+            listData.add(data.getString(1)) //it contains all the names of the contacts saved in the database,
+                                                        // so the contacts previously selected as the emergency ones
+        }
+
+        //create the list adapter and set the adapter and populate the listView
+        users_list.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listData)
+
+        //set click listener to the list view
+        users_list.setOnItemClickListener { adapterView: AdapterView<*>, view1: View, i: Int, l: Long ->
+                var name: String = adapterView.getItemAtPosition(i).toString()
+                var data: Cursor = mDatabaseHelper.getItemID(name) //get the ID associated witch that name
+                var itemID: Int = -1
+
+                while (data.moveToNext()) {
+                    itemID = data.getInt(0) // contains the ID of the contact selected from the listView
+                }
+                if (itemID > -1) {
+
+                    AlertDialog.Builder(this) // creates a pop up to ask the user if he wants to
+                                                    // delete the one he clicked on from the emergency contacts
+                        .setMessage("Do you want to delete this contact from the emergency ones?")
+                        .setPositiveButton("yes") { _, _ -> //if the user click on "yes", pass the ID and name of the
+                                                                // selected contact to the database to delete them
+                            mDatabaseHelper.deleteName(itemID,name)
+                        }
+                        .setNegativeButton("no") { dialog, _ ->
+                            dialog.dismiss() //if the user click on "no", the pop up closes without performing any action
+                        }
+                        .show()
+                } else { // if the user clicks on an item in the list that has already been deleted
+                    toastMessage("\n" +
+                            "this contact is no longer an emergency contact")
+               }
+
         }
     }
 }
+
